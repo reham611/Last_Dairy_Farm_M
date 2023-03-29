508d49309d3ed4354bdf9546ff732ed8d36b2fa5
reham611 <117862392+reham611@users.noreply.github.com>
2023-03-29 13:00:50 -0700

    change the " Date  " to " Vet Name"

--- Last_Dairy_Farm_M/CowHealth.Designer.cs
+++ Last_Dairy_Farm_M/CowHealth.Designer.cs
@@ -277,9 +277,9 @@ namespace Last_Dairy_Farm_M
             this.label8.Location = new System.Drawing.Point(1277, 216);
             this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(75, 31);
+            this.label8.Size = new System.Drawing.Size(129, 31);
             this.label8.TabIndex = 97;
-            this.label8.Text = "Date ";
+            this.label8.Text = "Vet Name";
             // 
             // label19
             // 
